68754a6c88a3aabdd6d679bbc317e2b2390683a1
BhanuSravya-Nissankararao <121043749+BhanuSravya-Nissankararao@users.noreply.github.com>
2022-12-28 11:07:16 +0530

    strata

---
+++ counter.js
@@ -0,0 +1,38 @@
+function updateCounter() {
+    const counter = document.getElementById('counter');
+    //increment the counter everytime user visits the page by storing in local storage
+    let count = localStorage.getItem('visiterCount');
+    /**
+     * if count is null then set count to 1
+    * else increment the count by 1 (Note: count is a string so convert it to number using parseInt)
+    */
+//    count =2;
+    if(count == String(null)){
+        count = 1;
+    }
+    else{
+        count=parseInt(count)+1;
+    }
+    localStorage.setItem('visiterCount', count);
+    counter.innerHTML = `Vistor Count: ${localStorage.getItem('visiterCount')}`;
+}
+updateCounter();
+// function updateLocation() {
+//     const location = document.getElementById('location');
+//     //increment the counter everytime user visits the page by storing in local storage
+//     let locat = localStorage.getItem('visiterLocation');
+//     /**
+//      * if count is null then set count to 1
+//     * else increment the count by 1 (Note: count is a string so convert it to number using parseInt)
+//     */
+// //    locat=5;
+//     if(locat == null){
+//         locat = 1;
+//     }
+//     else{
+//         locat++;
+//     }
+//     localStorage.setItem('visiterLocation', locat);
+//     location.innerHTML = `Vistor Location: ${localStorage.getItem('visiterLocation')}`;
+// }
+// updateLocation();
